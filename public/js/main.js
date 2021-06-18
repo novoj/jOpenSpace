699d067756c64703ccb15102f14a8b96204cd0b9
JNO <novotny@fg.cz>
2021-06-18 20:33:55 +0200

    Year 2021 - update.

--- public/js/main.js
+++ public/js/main.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
         });
         $('#mapCanvas').gmap('option', 'zoom', 14);
     });*/
-    var center = SMap.Coords.fromWGS84(49.1867294, 15.4482842);
+    var center = SMap.Coords.fromWGS84(15.4482842, 49.1867294);
     var m = new SMap(JAK.gel("mapCanvas"), center, 13);
     m.addDefaultLayer(SMap.DEF_BASE).enable();
     m.addDefaultControls();
